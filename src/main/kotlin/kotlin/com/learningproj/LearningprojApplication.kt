f5646792bb7d346a728237695ea68509fecfd6ef
igorthebarros <1igorhbarros@gmail.com>
2024-02-04 17:03:38 -0300

    fix last commit miss-commited changes

---
+++ src/main/kotlin/kotlin/com/learningproj/LearningprojApplication.kt
@@ -0,0 +1,11 @@
+package kotlin.com.learningproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningprojApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningprojApplication>(*args)
+}
---
+++ src/test/kotlin/kotlin/com/learningproj/LearningprojApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlin.com.learningproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningprojApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
